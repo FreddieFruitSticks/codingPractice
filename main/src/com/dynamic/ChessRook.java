c03723c52117b744d140e339167c5bfc2d7fd5b5
freddie <freddieodonnell@gmail.com>
2016-09-12 19:51:24 +0200

    chessrook

--- main/src/com/dynamic/ChessRook.java
+++ main/src/com/dynamic/ChessRook.java
@@ -2,7 +2,6 @@ package com.dynamic;
 
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -16,7 +15,7 @@ public class ChessRook {
         chessboard[1][1] = 1;
         for (int i = 0; i < numberOfInadmissibleBlocks; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), ",");
-            chessboard[Integer.parseInt(stringTokenizer.nextToken())][Integer.parseInt(stringTokenizer.nextToken())] = -1;
+            chessboard[Integer.valueOf(stringTokenizer.nextToken())][Integer.valueOf(stringTokenizer.nextToken())] = -1;
         }
         System.out.println(calcNumberOfPathsToEndBlock(chessboard));
     }
@@ -24,18 +23,30 @@ public class ChessRook {
     private static int calcNumberOfPathsToEndBlock(int[][] chessBoard) {
         for (int i = 1; i < chessBoard.length; i++) {
             for (int j = 1; j < chessBoard.length; j++) {
-                if (i == 1 && j == 1) {
-                    continue;
-                }
-                if (chessBoard[i - 1][j] != -1 && chessBoard[i][j - 1] != -1) {
-                    chessBoard[i][j] = chessBoard[i - 1][j] + chessBoard[i][j - 1];
-                } else if (chessBoard[i - 1][j] == -1) {
-                    chessBoard[i][j] = chessBoard[i][j - 1];
-                } else if (chessBoard[i][j - 1] == -1) {
-                    chessBoard[i][j] = chessBoard[i - 1][j];
+                if (chessBoard[i][j] != -1) {
+                    if (i == 1 && j == 1) {
+                        continue;
+                    }
+                    if (chessBoard[i - 1][j] != -1 && chessBoard[i][j - 1] != -1) {
+                        chessBoard[i][j] = chessBoard[i - 1][j] + chessBoard[i][j - 1];
+                    } else if (chessBoard[i - 1][j] == -1) {
+                        chessBoard[i][j] = chessBoard[i][j - 1];
+                    } else if (chessBoard[i][j - 1] == -1) {
+                        chessBoard[i][j] = chessBoard[i - 1][j];
+                    }
                 }
             }
         }
+        printMatrix(chessBoard);
         return chessBoard[8][8];
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
