788adf96e4431afb3bf466bc6410d9b18d35ba17
freddie <freddieodonnell@gmail.com>
2016-10-04 23:43:01 +0200

    prims

--- main/src/com/Graphs/FindMinSpanningTree.java
+++ main/src/com/Graphs/FindMinSpanningTree.java
@@ -14,20 +14,39 @@ public class FindMinSpanningTree {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("/home/freddie/Desktop/spanning"));
+        String line;
+        if ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            int numOfNodes = Integer.valueOf(st.nextToken());
+            int numOfEdges = Integer.valueOf(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int numOfNodes = Integer.valueOf(st.nextToken());
-        int numOfEdges = Integer.valueOf(st.nextToken());
 
-        int[][] graph = new int[numOfNodes][numOfNodes];
+            int[][] graph = new int[numOfNodes][numOfNodes];
 
-        for (int i = 0; i < numOfEdges; i++) {
-            st = new StringTokenizer(br.readLine());
-            graph[Integer.valueOf(st.nextToken()) - 1][Integer.valueOf(st.nextToken()) - 1] = Integer.valueOf(st.nextToken());
+            for (int i = 0; i < numOfEdges; i++) {
+                if ((line = br.readLine()) != null) {
+                    st = new StringTokenizer(line);
+                    String first = st.nextToken();
+                    String second = st.nextToken();
+                    String weight = st.nextToken();
+                    graph[Integer.valueOf(first) - 1][Integer.valueOf(second) - 1] = Integer.valueOf(weight);
+                    graph[Integer.valueOf(second) - 1][Integer.valueOf(first) - 1] = Integer.valueOf(weight);
+                } else {
+                    System.out.println("buffered Reader ate null line");
+                }
+            }
+            if ((line = br.readLine()) != null) {
+                st = new StringTokenizer(line);
+                int startNode = Integer.valueOf(st.nextToken()) - 1;
+//                printGraph(graph);
+//                System.out.println();
+                System.out.println(sumGraph(primsAlgo(graph, startNode)));
+            } else {
+                System.out.println("null final line");
+            }
+        } else {
+            System.out.println("null first line");
         }
-        st = new StringTokenizer(br.readLine());
-        int startNode = Integer.valueOf(st.nextToken()) - 1;
-        printGraph(primsAlgo(graph, startNode));
     }
 
     private static void printGraph(int[][] graph) {
@@ -40,5 +59,54 @@ public class FindMinSpanningTree {
             }
         }
     }
-    
+
+    private static int sumGraph(int[][] graph){
+        int sum = 0;
+        if (graph != null && graph.length > 0 && graph[0].length > 0) {
+            for (int[] ints : graph) {
+                for (int anInt : ints) {
+                    sum+=anInt;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int[][] primsAlgo(int[][] graph, int startNode) {
+        if (graph != null && graph.length > 0 && graph[0].length > 0) {
+            int[][] spanningTree = new int[graph.length][graph[0].length];
+            Set<Integer> visitedNodes = new HashSet<>();
+            Set<Integer> nodesToVisit = new HashSet<>();
+            for (int i = 0; i < graph.length; i++) {
+                nodesToVisit.add(i);
+            }
+            visitedNodes.add(startNode);
+            nodesToVisit.remove(startNode);
+
+
+            while (!nodesToVisit.isEmpty()) {
+                int min = -1;
+                int minStartNode = 0;
+                int minEndNode = 0;
+                for (Integer visitedNode : visitedNodes) {
+                    for (Integer nodeToVisit : nodesToVisit) {
+                        if (graph[visitedNode][nodeToVisit] < min && graph[visitedNode][nodeToVisit] != 0) {
+                            min = graph[visitedNode][nodeToVisit];
+                            minStartNode = visitedNode;
+                            minEndNode = nodeToVisit;
+                        } else if (min == -1 && graph[visitedNode][nodeToVisit] != 0) {
+                            min = graph[visitedNode][nodeToVisit];
+                            minStartNode = visitedNode;
+                            minEndNode = nodeToVisit;
+                        }
+                    }
+                }
+                spanningTree[minStartNode][minEndNode] = min;
+                visitedNodes.add(minEndNode);
+                nodesToVisit.remove(minEndNode);
+            }
+            return spanningTree;
+        }
+        return null;
+    }
 }
