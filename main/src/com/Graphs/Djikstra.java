c24dadac512c0a886540c5ad8cac1f63f551bf1f
freddie <freddieodonnell@gmail.com>
2016-10-09 14:26:48 +0200

    Djikstra

--- main/src/com/Graphs/Djikstra.java
+++ main/src/com/Graphs/Djikstra.java
@@ -25,8 +25,13 @@ public class Djikstra {
                 int vertex1 = Integer.valueOf(st.nextToken()) - 1;
                 int vertex2 = Integer.valueOf(st.nextToken()) - 1;
                 int edgeWeight = Integer.valueOf(st.nextToken());
-                graph[vertex1][vertex2] = edgeWeight;
-                graph[vertex2][vertex1] = edgeWeight;
+                if (graph[vertex1][vertex2] != -1 && edgeWeight < graph[vertex1][vertex2]) {
+                    graph[vertex1][vertex2] = edgeWeight;
+                    graph[vertex2][vertex1] = edgeWeight;
+                } else if (graph[vertex1][vertex2] == -1) {
+                    graph[vertex1][vertex2] = edgeWeight;
+                    graph[vertex2][vertex1] = edgeWeight;
+                }
 
             }
             st = new StringTokenizer(br.readLine());
@@ -40,13 +45,25 @@ public class Djikstra {
                 }
             }
             int[] minDistances = djikstra(graph, vertices, startVertex);
-            printArray(minDistances);
+            printArray(minDistances, startVertex);
+            if (i != numOfCases - 1) {
+                System.out.println();
+            }
         }
     }
 
-    private static void printArray(int[] array){
-        for (int i : array) {
-            System.out.println(i);
+    private static void printArray(int[] array, int startNode) {
+        for (int i = 0; i < array.length; i++) {
+            if (i != startNode) {
+                if(array[i] == Integer.MAX_VALUE || array[i] < 0){
+                    System.out.print(-1);
+                }else {
+                    System.out.print(array[i]);
+                }
+                if (i != array.length - 1) {
+                    System.out.print(" ");
+                }
+            }
         }
     }
 
@@ -57,16 +74,19 @@ public class Djikstra {
             notVisited[i] = 1;
         }
         visited[startVertex] = 1;
-        notVisited[startVertex] = 0;
+        notVisited[startVertex] = -1;
 
         while (!isEmptyArray(notVisited)) {
             for (int i = 0; i < graph.length; i++) {
-                if (graph[startVertex][i] >= 0 && graph[startVertex][i] + vertices[startVertex] < vertices[i]
-                        && visited[i] != 1) {
+                if (graph[startVertex][i] >= 0 && graph[startVertex][i] + vertices[startVertex] < vertices[i]) {
                     vertices[i] = graph[startVertex][i] + vertices[startVertex];
                 }
             }
             startVertex = findMin(vertices, visited);
+
+            if (startVertex == 75){
+                int o = 0;
+            }
             visited[startVertex] = 1;
             notVisited[startVertex] = -1;
         }
@@ -75,7 +95,7 @@ public class Djikstra {
 
     private static boolean isEmptyArray(int[] array) {
         for (int i : array) {
-            if (i > 0) {
+            if (i > -1) {
                 return false;
             }
         }
@@ -85,9 +105,9 @@ public class Djikstra {
     private static int findMin(int[] array, int[] visited) {
         if (array != null && array.length > 0) {
             int min = Integer.MAX_VALUE;
-            int minIndex = 0;
-            for (int i =0; i < array.length; i++) {
-                if (array[i] < min && array[i] != 0 && visited[i] == 0) {
+            int minIndex = -1;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] <= min && visited[i] == 0) {
                     min = array[i];
                     minIndex = i;
                 }
